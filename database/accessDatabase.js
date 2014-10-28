696819a4f71a0ed4af4488d1ccbe482e53549bc2
Simon Grønborg <groenborg27@gmail.com>
2014-10-28 15:13:13 +0100

    Router and database query for orders added
    	Commit by: Simon

--- database/accessDatabase.js
+++ database/accessDatabase.js
@@ -1,43 +1,30 @@
 var mongoose = require('mongoose');
-var schema = mongoose.Schema;
 var model = require('./model');
-
-
 var dbUrl = "mongodb://localhost/northwind";
 
-function connectToDatabase(){
+
+var connect = function connectToDatabase() {
     mongoose.connect(dbUrl);
-}
+};
 
-function getAllOrders(){
-    var orderSchema = new schema({
-        orderID : Number,
-        customerID : Number,
-        employeeID : Number,
-        orderDate : String,
-        requiredDate : String,
-        shippedDate : String,
-        shipVia : String,
-        freight : String,
-        shipName : String,
-        shipAddress : String,
-        shipCity : String,
-        shipRegion : String,
-        shipPostalCode : String,
-        shipCountry : String
-    });
+var getAllOrders = function (callback) {
+    model.OrderModel.find({}, function (err, data) {
+        if (err) {
+            return callback(err);
+        }
+        return callback(null, data);
+    })
+};
 
-    var orderModel = mongoose.model('Order', orderSchema);
 
-    var allOrders = orderModel.find(function (err, orders) {
-        if (err) return console.error(err);
-        return orders;
-    });
-    return allOrders;
-}
-
-function closeDatabaseConnection(){
+var close = function () {
     mongoose.connection.close();
-}
+};
 
 
+module.exports = {
+    connect: connect,
+    close: close,
+    getAllOrders: getAllOrders
+    //getOrderDetails:
+};
--- routes/orders.js
+++ routes/orders.js
@@ -1,11 +1,17 @@
 var express = require('express');
+var db = require('../database/accessDatabase');
 var router = express.Router();
 
-
 router.get('/', function (request, response) {
+    db.connect();
+    db.getAllOrders(function (err, data) {
+        if (err) {
+            response.send("No data was found");
+        }
+        response.send(data);
 
-
-    response.render('index', {data: [1, {name:"simon"}, 3, 4, 5, 6, 67, 7, 8, 8]});
+    });
+    //response.render('index', {data: [1, {name: "simon"}, 3, 4, 5, 6, 67, 7, 8, 8]});
 
 });
 
