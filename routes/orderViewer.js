95f1534b208800ae6cc9ddeaa9c01082a39db236
thomashed <thomashj93@gmail.com>
2014-10-28 21:28:55 +0100

    - orderViewer.jade now has wirking loop to fill table - ready for data object - added navbar ready for use (maybe temp) - created dummy-object in orderViewer.js (remove) by: Thomas

--- routes/orderViewer.js
+++ routes/orderViewer.js
@@ -5,7 +5,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(request, response){
-    response.render('orderViewer', {title: 'Order Viewer'});
+    response.render('orderViewer', {title: 'Order Viewer', data: [{name:"Yrsa", age: 34}, {name:"Hank", age: 42},{name:"Ole", age: 12}]});
 });
 
 module.exports = router;
